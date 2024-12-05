a771898ab9abeeedb8afcc46a719ef678d8f1670
Phu Kyaw Win <94231078+phukyawwin@users.noreply.github.com>
2024-12-06 02:08:31 +0700

    change design

--- AppointmentBookingSystem/AppointmentBookingSystem.Web/obj/Debug/net8.0/AppointmentBookingSystem.Web.AssemblyInfo.cs
+++ AppointmentBookingSystem/AppointmentBookingSystem.Web/obj/Debug/net8.0/AppointmentBookingSystem.Web.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AppointmentBookingSystem.Web")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a244de7783054e6318f3d5e8332bb741165a0edd")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9877cfc90d74290b6b0daf2eac7ab0f02f5ccdc0")]
 [assembly: System.Reflection.AssemblyProductAttribute("AppointmentBookingSystem.Web")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AppointmentBookingSystem.Web")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
