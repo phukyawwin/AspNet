e6ab6fd7406341da973a9d1ff7d77f0e5abfd313
Phu Kyaw Win <94231078+phukyawwin@users.noreply.github.com>
2024-12-25 13:09:18 +0630

    feat : change model

---
+++ BlazorChatApp/BlazorChatApp.Domain/Entities/Chat.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlazorChatApp.Domain.Entities
+{
+    public class Chat
+    {
+        [Required]
+        public string Message { get; set; }
+        [Required]
+        public string? UserName { get; set; }
+        public DateTime Date { get; set; } = DateTime.Now;
+}
+}
--- BlazorChatApp/BlazorChatApp/Hubs/ChatHub.cs
+++ BlazorChatApp/BlazorChatApp/Hubs/ChatHub.cs
@@ -1,10 +1,11 @@
-﻿using Microsoft.AspNetCore.SignalR;
+﻿using BlazorChatApp.Domain.Entities;
+using Microsoft.AspNetCore.SignalR;
 
 namespace BlazorChatApp.Hubs
 {
     public class ChatHub:Hub
     {
-        public async Task SendMessage(string userName,string message,DateTime date)
-        => await Clients.All.SendAsync("ReceiveMessage",userName, message, date);
+        public async Task SendMessage(Chat chat)
+        => await Clients.All.SendAsync("ReceiveMessage", chat);
     }
 }
